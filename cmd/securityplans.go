65cfddad096a23b9c90b3fca6c465f3aa74295f0
jon ceanfaglione <jon@>
2021-01-01 13:52:17 -0500

    updated help output

--- cmd/securityplans.go
+++ cmd/securityplans.go
@@ -24,13 +24,8 @@ import (
 // securityplansCmd represents the securityplans command
 var securityplansCmd = &cobra.Command{
 	Use:   "securityplans",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Combined with commands like `Get`, enables the retrieval of security plan information.",
+	Long: `Combined with commands like 'Get', enables the retrieval of security plan information.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("securityplans called")
 	},
